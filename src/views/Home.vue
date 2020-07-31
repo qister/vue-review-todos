b20316411b994402e1bdeec24c593b28b3847f6b
qister <qistersick@gmail.com>
2020-07-31 12:46:17 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>HomePage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed aperiam ad consequuntur harum. A, adipisci nam accusantium, amet iure illo nesciunt, impedit </p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
